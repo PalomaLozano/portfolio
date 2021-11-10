0941c4ad135e40be51bdec3760b0267e191b3e28
Paloma Lozano <palomalogu9@gmail.com>
2021-11-10 12:43:30 +0100

    added formspree

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -16,27 +16,44 @@ const Contact = () => {
         <h2 className="letstalk">Let´s Talk!</h2>
         <img src={talk} alt="talk img" className="imgContact" />
         <div className="contactForm">
-          <form id="form" action="" className="form">
-            <label for="name">Name:</label>
+          <form
+            className="form"
+            action="https://formspree.io/f/mvodeprg"
+            method="post"
+          >
+            <label for="Name" className="label">
+              Name
+            </label>
             <input
+              placeholder="Write your name..."
               type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              required
+              id="Name"
+              name="Name"
+              className="input"
             />
-            <label for="name">Email:</label>
+            <label className="label" for="emailAddress">
+              E-mail
+            </label>
             <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Email"
-              required
+              placeholder="Write your E-mail..."
+              type="email"
+              id="emailAddress"
+              name="emailAddress"
+              className="input"
             />
-            <label for="name">Message:</label>
-            <textarea id="message" name="message"></textarea>
-            <a href="mailto:palomalogu9@gmail.com">Contactar</a>
-            <button type="submit"></button>
+            <label for="message" className="label">
+              Message
+            </label>
+            <input
+              placeholder="Write your message"
+              type="text"
+              id="comments"
+              name="comments"
+              size="15"
+              maxlength="100"
+              className="inputMessage"
+            />
+            <input type="submit" value="Enviar" />
           </form>
         </div>
       </section>
